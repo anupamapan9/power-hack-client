9f423ae9fd83eab3f3fd1c281b3fba023b10b837
ANUPAM MITRA <anupamapan0905@gmail.com>
2022-06-30 10:12:08 +0600

    initialize project

---
+++ src/Components/Common/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='px-20 bg-gray-400 flex justify-between h-16 items-center text-xl'>
+            <div>Power Hack</div>
+            <div>Paid Total: 0</div>
+        </header>
+    );
+};
+
+export default Header;
