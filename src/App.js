beb09f2a60f2a0d69a0539d664633f785a29ef1c
ANUPAM MITRA <anupamapan0905@gmail.com>
2022-07-01 08:21:15 +0600

    login & register page designed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Billing from './Components/Billing/Billing';
 import Header from './Components/Common/Header';
+import Login from './Components/Users/Login';
+import Register from './Components/Users/Register';
 
 function App() {
   return (
     <main>
       <Header />
-      <Billing />
+      <Routes>
+        <Route path='/' element={<Billing />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+      </Routes>
     </main>
   );
 }
